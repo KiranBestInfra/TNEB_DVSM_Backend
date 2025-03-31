d3e68036ec2fb728aa587d3e2450b02452033a53
Chandrika <chandrika.bestinfra@gmail.com>
2025-03-31 12:15:27 +0530

    Substations Added

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,15 +3,16 @@ import authRoutes from '../auth.js';
 import regionRoutes from '../regions.js';
 // import demoRoutes from '../demo.js';
 import consumerRoutes from '../consumer.js';
-import edcRoutes from '../edc.js'
-
+import edcRoutes from '../edc.js';
+// import substationRoutes from '../substation.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/regions', regionRoutes);
 router.use('/consumer', consumerRoutes);
-router.use('/edcs',edcRoutes);
+router.use('/edcs', edcRoutes);
+//router.use('/substation', substationRoutes);
 // router.use('/demo', demoRoutes);
 
 export default router;
