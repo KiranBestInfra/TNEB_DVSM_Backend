02e1fbd97a4b2e9952e1b8306e8c6c28fe32fadd
Chandrika <chandrika.bestinfra@gmail.com>
2025-04-05 14:52:43 +0530

    feeders name for each edc

--- routes/edc.js
+++ routes/edc.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { getEDCWidgets } from '../controllers/main/edcsController.js';
 import { getSubstationWidgets } from '../controllers/main/substationController.js';
+import { getFeedersNamesByEdcNameHandler } from '../controllers/main/feederController.js';
 
 const router = express.Router();
 
 router.get('/widgets/:region', getEDCWidgets);
 router.get('/widgets/:region/substations', getSubstationWidgets);
+router.get('/:edc/feeders', getFeedersNamesByEdcNameHandler);
 
 export default router;
 
